554970d682572d5c1e96353d2d2457d55b2538f6
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-07-30 09:35:43 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,11 +47,13 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongScore();
     }
 
-    runGame(calculatedAnswer[1]);
+    runGame(calculatedAnswer[1]); 
 }
 
 /**
@@ -70,13 +72,20 @@ function calculateCorrectAnswer() {
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
 }
-
+/**
+ * Gets the current score from the DOM and it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and it by 1
+ */
 function incrementWrongScore() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
